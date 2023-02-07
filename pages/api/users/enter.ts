b6cb0f22d48eec5a505e353d8a04d53c8523a1d9
jellie <talentforest0501@gmail.com>
2023-02-08 06:44:15 +0900

    send email token setting

--- pages/api/users/enter.ts
+++ pages/api/users/enter.ts
@@ -2,6 +2,7 @@ import twilio from 'twilio';
 import client from '@/libs/server/client';
 import withHandler, { ResponseType } from '@/libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
+import smtpTransport from '@/libs/server/email';
 
 const myPhoneNum = process.env.MY_PHONE_NUM;
 const msgSID = process.env.MESSAGING_SERVICE_SID;
@@ -40,6 +41,28 @@ async function handler(
       body: `Your login token is ${payload}`,
     });
     console.log(message);
+  }
+  if (email) {
+    const mailOptions = {
+      from: process.env.MAIL_ID,
+      to: process.env.MAIL_ID,
+      subject: 'Nomad Carrot Authentication Email',
+      text: `Authentication Code : ${payload}`,
+    };
+    const result = await smtpTransport.sendMail(
+      mailOptions,
+      (error, responses) => {
+        if (error) {
+          console.log(error);
+          return null;
+        } else {
+          console.log(responses);
+          return null;
+        }
+      }
+    );
+    smtpTransport.close();
+    console.log(result);
   }
   return res.json({ ok: true });
 }
